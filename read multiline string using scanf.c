8c4d478435aa255655a6bd409aeae22dc569e0ac
SANEEP P P <61908137+saneep-debug@users.noreply.github.com>
2021-10-30 20:59:24 +0530

    Create read multiline string using scanf.c

---
+++ read multiline string using scanf.c
@@ -0,0 +1,15 @@
+ //Program to read multiline string using scanf
+#include<stdio.h>
+int main(void) {
+  
+  char str[100];
+  // reading multline string with '.' as a delimiter
+  scanf("%[^.]",str);
+  
+  printf("%s",str);
+  
+  return 0;
+  
+  
+  
+} 
